9097d714438e4b7732762632555801cdd98bcb96
Connor Finnegan <connorfinnegan16@gmail.com>
2018-11-09 04:03:31 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
         if new_hash[name][attribute].nil?
           new_hash[name][attribute] = []
         end
-        new_ha
+        new_hash[name][attribute] << k 
       end
     end
   end
