79835e9431ea03775ae345b6f3f63ada7b2b6fc4
Connor Finnegan <connorfinnegan16@gmail.com>
2018-11-09 04:02:24 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |attribute, attribute_data|
     attribute_data.each do |k, v|
+      binding.pry
       v.each do |name|
         if new_hash[name].nil?
           new_hash[name] = {}
@@ -15,5 +16,4 @@ def nyc_pigeon_organizer(data)
     end
   end
   new_hash
-  binding.pry
 end
